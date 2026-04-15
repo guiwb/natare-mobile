31af0d7df8054ae6927e871118c9412fd91068a4
gui <gui.wb@hotmail.com>
2026-04-15 20:22:28 -0300

    feat(home): improve styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,22 @@
 import { useAuth } from '@/contexts/AuthProvider';
 import { StyleSheet, View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { Button, Text, useTheme } from 'react-native-paper';
 
 export default function HomeScreen() {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
+  const theme = useTheme();
 
   return (
     <View style={styles.container}>
-      <Text>Home tab</Text>
-      <Button mode="contained" onPress={logout}>
-        Logout
+      <Text variant="titleMedium">Olá, seja bem-vindo {user?.name}!</Text>
+      <Button
+        mode="contained"
+        buttonColor={theme.colors.errorContainer}
+        textColor={theme.colors.onErrorContainer}
+        icon="logout"
+        onPress={logout}
+      >
+        Sair
       </Button>
     </View>
   );
